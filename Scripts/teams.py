9f664c199b5e19d0597c471f8352a979694b3588
BuildTools <unconfigured@null.spigotmc.org>
2023-03-10 13:16:19 -0500

    Started teams.py

---
+++ Scripts/teams.py
@@ -0,0 +1,11 @@
+#Import libraries
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import os
+
+#Import Team Data
+path = os.path.dirname(os.getcwd())
+
+teams = pd.read_csv(path+'/data/teams.csv')
